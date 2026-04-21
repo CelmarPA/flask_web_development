200be9ed179ee0d019d65648719dad08d811fc54
Celmar Pereira <celmarpereiradeandrade@gmail.com>
2026-04-21 11:19:44 -0300

    Example 3-9

--- hello.py
+++ hello.py
@@ -21,3 +21,12 @@ def browser() -> str:
 
     return f"<p>Your browser is {user_agent}</p>"
 
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
