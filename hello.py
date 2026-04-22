3424c09b5be70c6cad85fef1464b0057f672b239
Celmar Pereira <celmarpereiradeandrade@gmail.com>
2026-04-22 13:18:12 -0300

    Example 5-5

--- hello.py
+++ hello.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy.orm import Mapped, mapped_column, relationship, DynamicMapped
 from datetime import datetime, UTC
 from typing import Tuple
 from secrets import token_hex
@@ -36,7 +36,7 @@ class Role(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(unique=True)
 
-    users: Mapped[list["User"]] = relationship(backref="role")
+    users: DynamicMapped[list["User"]] = relationship(backref="role", lazy="dynamic")
 
     def __repr__(self):
         return f"<Role {self.name!r}>"
@@ -49,7 +49,7 @@ class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
     username: Mapped[str] = mapped_column(unique=True, index=True)
 
-    role_id: Mapped[int] = mapped_column(ForeignKey("roles.id"))
+    role_id: Mapped[int] = mapped_column(ForeignKey("roles.id"),nullable=True)
 
     def __repr__(self):
         return f"<User {self.username!r}>"
@@ -60,17 +60,24 @@ def index() -> str:
     form: NameForm = NameForm()
 
     if form.validate_on_submit():
-        old_name: str = session.get("name")
+        user: str = User.query.filter_by(username=form.name.data).first()
+        print(user)
+        if user is None:
+            user: User = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session["known"] = False
 
-        if old_name is not None and old_name != form.name.data:
-            flash("Looks like you have changed your name!")
+        else:
+            session["known"] = True
 
-        session["name"]: str = form.name.data
+        session["name"] = form.name.data
+        form.name.data = ""
 
         return redirect(url_for("index"))
 
     return render_template("index.html", form=form, name=session.get("name"),
-                           current_time=datetime.now(UTC))
+                           known=session.get("known", False), current_time=datetime.now(UTC))
 
 
 @app.route("/user/<name>")
