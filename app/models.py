220fb4cacd19ca6daa9cc4b07dde5ccd75835a65
Celmar Pereira <celmarpereiradeandrade@gmail.com>
2026-04-30 11:47:31 -0300

    Example 8-1

--- app/models.py
+++ app/models.py
@@ -1,6 +1,7 @@
 from . import db
 from sqlalchemy import ForeignKey
 from sqlalchemy.orm import Mapped, mapped_column, relationship, DynamicMapped
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class Role(db.Model):
@@ -12,7 +13,7 @@ class Role(db.Model):
 
     users: DynamicMapped[list["User"]] = relationship(backref="role", lazy="dynamic")
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"<Role {self.name!r}>"
 
 
@@ -25,5 +26,18 @@ class User(db.Model):
 
     role_id: Mapped[int] = mapped_column(ForeignKey("roles.id"), nullable=True)
 
-    def __repr__(self):
+    password_hash: Mapped[str] = mapped_column(nullable=False)
+
+    def __repr__(self) -> str:
         return f"<User {self.username!r}>"
+
+    @property
+    def password(self) -> None:
+        raise AttributeError("password is not a readable attribute")
+
+    @password.setter
+    def password(self, password) -> None:
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password) -> None:
+        return check_password_hash(self.password_hash, password)
